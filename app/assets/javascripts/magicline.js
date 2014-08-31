b9a29c71712c1cf032b8376a9860322c056f0dcd
Artem Aminov <artemaminov@gmail.com>
2014-09-01 00:21:51 +0600

    add second magicline

--- app/assets/javascripts/magicline.js
+++ app/assets/javascripts/magicline.js
@@ -1,27 +1,54 @@
-$(document).ready(function() {
-    var o = $('#nav li'),
-        ml = $('.menu-cursor'),
-        c = $('.navigation'),
+$(function() {
+    var menuElements1 = $('#nav li'),
+        menuElements2 = $('.ui-tabs-nav li'),
+        menuContainer1 = $('.navigation'),
+        menuContainer2 = $('.portfolio'),
+        menuCursor1 = $('.menu-cursor1'),
+        menuCursor2 = $('.menu-cursor2'),
         cursorIsOut = false;
 
-    ml.offset({left: 0 - ml.width()});
-
-    o.on('mouseover mouseout', function(event) {
+    var cursorIn = function(event, obj, cursor) {
         cursorIsOut = true;
-        var t = $(this);
-        ml.addClass('transition');
-        ml.offset({left: t.offset().left + ml.width() / 2});
+        var t = $(obj),
+            cursorMargin = 0;
+
+        if (cursor == menuCursor1) {
+            cursorMargin = cursor.width() / 2;
+        }
+
+        cursor.addClass('transition');
+        cursor.offset({left: t.offset().left + cursorMargin});
         event.stopPropagation();
+        },
+
+        cursorOut = function (event, container, cursor) {
+            if (cursorIsOut) {
+                cursor.offset({left: container.width()});
+                cursor.one('transitionend', function() {
+                    cursor.removeClass('transition');
+                    cursor.offset({left: 0 - cursor.width()});
+                });
+                cursorIsOut = false;
+            }
+        };
+
+    menuCursor1.offset({left: 0 - menuCursor1.width()});
+    menuCursor2.offset({left: 0 - menuCursor2.width()});
+
+    menuElements1.on('mouseover mouseout', function() {
+        cursorIn(event, this, menuCursor1)
+    });
+    menuElements2.on('mouseover mouseout', function() {
+        cursorIn(event, this, menuCursor2)
     });
 
-    c.on('mouseover', function() {
-        if (cursorIsOut) {
-            ml.offset({left: c.width()});
-            ml.one('transitionend', function() {
-                ml.removeClass('transition');
-                ml.offset({left: 0 - ml.width()});
-            });
-            cursorIsOut = false;
-        }
+    menuContainer1.on('mouseover', function() {
+        cursorOut(event, menuContainer1, menuCursor1)
     });
+    menuContainer2.on('mouseover', function() {
+        cursorOut(event, menuContainer2, menuCursor2)
+    });
+
+    
+
 });
