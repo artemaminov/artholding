9e04d260cf4bed290fe63b8a2346cf2a3945f776
Artem Aminov <artemaminov@gmail.com>
2014-09-07 19:40:33 +0600

    add 3rd project browsing modal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,35 +2,35 @@ class HomeController < ApplicationController
 
   def index
     respond_to do |format|
-
       format.html {
         @groups = Group.all
         @projects = Project.all
         @clients = Client.all
       }
-
       format.js {
         @project = Project.find params[:id]
         @group = params[:group]
       }
-
     end
   end
 
   def zoom
     @project = Project.find params[:id]
     @group = params[:group]
-    render partial: 'shared/project_zoom'
+    respond_to do |format|
+      format.html {
+        render partial: 'shared/project_zoom'
+      }
+      format.js
+    end
   end
 
   def order
     respond_to do |format|
-
       format.js {
         Order.complete(params[:order]).deliver
         render status: :ok
       }
-
     end
   end
 
