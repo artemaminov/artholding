b9c46e3c9561ba87fede9e1d582bd2a669f491c0
Artem Aminov <artemaminov@gmail.com>
2014-09-15 12:58:10 +0600

    fix merge fail

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
   has_attached_file :carousel
   has_attached_file :main
 
-  validates :title, :about, :finished_at, :group_id, :main, presence: true
+  validates :title, :about, :finished_at, :group_id, :main, :position, :group_position, presence: true
   validates_attachment :preview, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   validates_attachment :carousel, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   validates_attachment :main, presence: true, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/x-shockwave-flash"] }
@@ -16,7 +16,7 @@ class Project < ActiveRecord::Base
   SWF_DROP = "and swf_width ISNULL"
 
   scope :carouselled, -> { where is_in_carousel: true }
-  scope :of_group, ->(group_id) { where group_id: group_id }
+  scope :of_group, ->(group_id) { where(group_id: group_id).order(group_position: :asc) }
   scope :recent, ->(amount) { limit amount }
 
   def self.find_neighbours id, group, zoom
@@ -26,21 +26,31 @@ class Project < ActiveRecord::Base
     project
   end
 
-  def prev_project group, zoom
-    next_project = Project.where(["id > :id #{SWF_DROP if zoom}", id: self.id])
-    next_project = next_project.of_group(self.group.id) if group == 'true'
-    next_project = next_project.order(id: :asc).first
+  def next_project group, zoom
+    if group == 'true'
+      next_project = Project.where(["group_position > :group_position #{SWF_DROP if zoom}", group_position: self.group_position])
+      next_project = next_project.of_group(self.group.id).first
+      return nil if next_project.blank? || next_project.group_position < self.group_position
+    else
+      next_project = Project.where(["position > :position #{SWF_DROP if zoom}", position: self.position])
+      next_project = next_project.order(position: :asc).first
+      return nil if next_project.blank? || next_project.position < self.position
+    end
 
-    return nil if next_project.blank? || next_project.id < self.id
     next_project
   end
 
-  def next_project group, zoom
-    prev_project = Project.where(["id < :id #{SWF_DROP if zoom}", id: self.id])
-    prev_project = prev_project.of_group(self.group.id) if group == 'true'
-    prev_project = prev_project.order(id: :desc).first
+  def prev_project group, zoom
+    if group == 'true'
+      prev_project = Project.where(["group_position < :group_position #{SWF_DROP if zoom}", group_position: self.group_position])
+      prev_project = prev_project.of_group(self.group.id).first
+      return nil if prev_project.blank? || prev_project.group_position > self.group_position
+    else
+      prev_project = Project.where(["position < :position #{SWF_DROP if zoom}", position: self.position])
+      prev_project = prev_project.order(position: :desc).first
+      return nil if prev_project.blank? || prev_project.position > self.position
+    end
 
-    return nil if prev_project.blank? || prev_project.id > self.id
     prev_project
   end
 
